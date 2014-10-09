f03258ff9a43390e1ba99aa3b39631624345bac3
Mike Gehard <mike.gehard@gmail.com>
2014-10-08 21:04:54 -0600

    Whitespace

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,20 @@
-var app = angular.module("app", []).run(function($rootScope) {
-  // adds some basic utilities to the $rootScope for debugging purposes
-  $rootScope.log = function(thing) {
-    console.log(thing);
-  };
+var app = angular.module("app", []).run(function ($rootScope) {
+    // adds some basic utilities to the $rootScope for debugging purposes
+    $rootScope.log = function (thing) {
+        console.log(thing);
+    };
 
-  $rootScope.alert = function(thing) {
-    alert(thing);
-  };
+    $rootScope.alert = function (thing) {
+        alert(thing);
+    };
 });
 
-app.factory('greeting', function() {
+app.factory('greeting', function () {
     return {
         content: "Hello, World!"
     };
 });
 
-app.controller('MainController', ['$scope', 'greeting', function($scope, greeting) {
+app.controller('MainController', ['$scope', 'greeting', function ($scope, greeting) {
     $scope.greeting = greeting.content;
 }]);
